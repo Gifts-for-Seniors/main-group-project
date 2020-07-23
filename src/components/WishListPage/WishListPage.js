9fa554bef4e3500627baa1fe489f89f588acdfce
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-07-23 11:32:28 -0500

    added code to display items on DOM in list form

--- src/components/WishListPage/WishListPage.js
+++ src/components/WishListPage/WishListPage.js
@@ -7,6 +7,36 @@ class WishList extends Component {
         return (
             <div>
                 <h1>WishList</h1>
+
+                <div className="highPriorityItems">
+                    <h2>High Priority Items</h2>
+
+                    {/* Mapping through our item reducer to display items marked as high priority */}
+                    {this.props.state.items.map((item) => {
+                        if(item.priority === true){
+                            return <ul key={item.id}>{item.item}</ul>
+                        }
+                    })
+                }
+                </div>
+
+                <div className="regularPriorityItems">
+                    <h2>Other Items</h2>
+
+                    {/* Mapping through our item reducer to display remaining items */}
+                    {this.props.state.items.map((item) => {
+                        if (item.priority === false) {
+                            return <ul key={item.id}>{item.item}</ul>
+                        }
+                    })
+                    }
+                </div>
+
+                <div>
+                    <button>Click here for drop-Off locations</button>
+                    <button>Click to shop our Amazon wishList</button>
+                </div>
+                
             </div>
         );
     }
