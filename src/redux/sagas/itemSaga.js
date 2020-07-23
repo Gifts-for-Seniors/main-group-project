2bfdedf3d7d2be3e11c5b69f62d2820214ed77a9
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-23 11:46:44 -0500

    Local commit

--- src/redux/sagas/itemSaga.js
+++ src/redux/sagas/itemSaga.js
@@ -1,31 +1,24 @@
-import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
-
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
 
 //GET WISHLIST
 function* fetchList(action) {
-    const id = action.payload
-    try {
-        const response = yield axios.get(`/api/wishlist`);
-        yield put({ type: 'SET_LIST', payload: response.data });
-    } catch (error) {
-        console.log('error getting details', error)
-    }
+  try {
+    const response = yield axios.get(`/api/wishlist`);
+    yield put({ type: "SET_LIST", payload: response.data });
+  } catch (error) {
+    console.log("error getting details", error);
+  }
 }
 
 //POST A NEW ITEM
 
-
 //UPDATE ITEM DESCRIPTION
-
 
 //UPDATE THE PRIORITY
 
-
-
-
 function* ItemSaga() {
-    yield takeLatest('FETCH_LIST', fetchList);
+  yield takeLatest("FETCH_LIST", fetchList);
 }
 
-export default ItemSaga
+export default ItemSaga;
