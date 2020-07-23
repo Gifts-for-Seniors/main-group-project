beb2b858233c72233004d12792a648486cb04bce
Your Name <nailaj123@gmail.com>
2020-07-23 11:38:31 -0500

    changed axios get

--- src/redux/sagas/itemSaga.js
+++ src/redux/sagas/itemSaga.js
@@ -6,16 +6,17 @@ function* fetchList(action) {
     const id = action.payload
     console.log('in payload', id)
     try {
-        const response = yield axios.get(`/select/${id}`);
+        const response = yield axios.get(`/api/wishlist`);
         yield put({ type: 'SET_LIST', payload: response.data });
     } catch (error) {
         console.log('error getting details', error)
     }
 }
 
-
 function* ItemSaga() {
     yield takeLatest('FETCH_LIST', fetchList);
 }
+
+
 
 export default ItemSaga
