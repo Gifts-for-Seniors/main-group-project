dce0be5ccd39a9a0de76acbf125738a6d2ed2266
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-24 12:17:20 -0500

    Push

--- src/components/WishListAdmin/WishListAdmin.js
+++ src/components/WishListAdmin/WishListAdmin.js
@@ -5,140 +5,169 @@ import StyledCheckbox from "../ButtonStyles/Checkbox";
 import LogOutButton from "../LogOutButton/LogOutButton";
 
 class WishListAdmin extends Component {
+  state = {
+    item: "",
+    itemToEdit: 0,
+    itemDescription: "",
+    // itemPriority: true
+  };
 
-    state = {
-        item: '',
-        itemToEdit: 0,
-        itemDescription: '',
-        // itemPriority: true
-    }
+  editItem = (id, description) => {
+    console.log("in edit", id, description);
 
-    editItem = (id, description) => {
-        console.log('in edit', id, description)
+    this.setState({
+      ...this.state,
+      itemToEdit: id,
+      itemDescription: description,
+    });
+  };
 
-        this.setState({
-            ...this.state,
-            itemToEdit: id,
-            itemDescription: description
-        })
-    }
+  cancelEdit = () => {
+    this.setState({
+      ...this.state,
+      itemToEdit: 0,
+    });
+  };
 
-    cancelEdit = () => {
-        this.setState({
-            ...this.state,
-            itemToEdit: 0
-        })
-    }
+  trackEdit = (event) => {
+    this.setState({
+      itemDescription: event.target.value,
+    });
+  };
 
-    trackEdit = (event) => {
-        this.setState({
-            itemDescription: event.target.value
-        })
-    }
+  saveEdit = () => {
+    this.props.dispatch({
+      type: "EDIT_ITEM",
+      payload: this.state,
+    });
 
-    saveEdit = () => {
+    this.setState({
+      ...this.state,
+      itemToEdit: 0,
+      itemDescription: "",
+    });
+  };
 
-        this.props.dispatch({
-            type: 'EDIT_ITEM',
-            payload: this.state
-        })
-
-        this.setState({
-            ...this.state,
-            itemToEdit: 0,
-            itemDescription: ''
-        })
-    }
-
-    updatePriority = (item) => {
-        console.log(item);
-        let data = {
-            id: item.id,
-            priority: item.priority,
-        };
-        this.props.dispatch({
-            type: "UPDATE_PRIORITY",
-            payload: data,
-        });
+  updatePriority = (item) => {
+    console.log(item);
+    let data = {
+      id: item.id,
+      priority: item.priority,
     };
-    deleteItem = (id) => {
-        this.props.dispatch({
-            type: 'DELETE_ITEM', payload: id,
-        })
-        console.log('payload', id)
-    }
+    this.props.dispatch({
+      type: "UPDATE_PRIORITY",
+      payload: data,
+    });
+  };
+  deleteItem = (id) => {
+    this.props.dispatch({
+      type: "DELETE_ITEM",
+      payload: id,
+    });
+    console.log("payload", id);
+  };
 
-    render() {
-        return (
-            <div className="adminView">
-                <h1>Admin WishList</h1>
+  render() {
+    return (
+      <div className="adminView">
+        <h1>Admin WishList</h1>
 
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Edit</th>
-                            <th>Select High Priority</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Edit</th>
+              <th>Select High Priority</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
 
-                    <tbody>
-                        {/* Mapping through our item reducer to display items marked as high priority */}
-                        {this.props.state.list.map((item) => {
-                            if (item.id === this.state.itemToEdit) {
+          <tbody>
+            {/* Mapping through our item reducer to display items marked as high priority */}
+            {this.props.state.list.map((item) => {
+              if (item.id === this.state.itemToEdit) {
+                return (
+                  <tr key={item.id}>
+                    <td>
+                      <input
+                        size="125"
+                        type="text"
+                        value={this.state.itemDescription}
+                        onChange={this.trackEdit}
+                      ></input>
+                    </td>
+                    <td>
+                      <StyledButton
+                        value={item.id}
+                        onClick={(event) => this.saveEdit(event)}
+                      >
+                        Save
+                      </StyledButton>
+                      <StyledButton value={item.id} onClick={this.cancelEdit}>
+                        Cancel
+                      </StyledButton>
+                    </td>
+                    <td>
+                      <input type="checkbox" value="true" />
+                    </td>
+                    <td>
+                      <button value={item.id}>Delete</button>
+                    </td>
+                  </tr>
+                );
+              } else
+                return (
+                  <tr key={item.id}>
+                    <td>{item.item}</td>
+                    <td>
+                      <StyledButton
+                        value={item.id}
+                        onClick={() => this.editItem(item.id, item.item)}
+                      >
+                        Edit
+                      </StyledButton>
+                    </td>
+                    <td>
+                      <input
+                        onChange={() => {
+                          this.updatePriority(item);
+                        }}
+                        type="checkbox"
+                        checked={item.priority}
+                      />
+                    </td>
 
-                                return <tr key={item.id}>
-                                    <td><input size="125" type="text" value={this.state.itemDescription} onChange={this.trackEdit}></input></td>
-                                    <td><StyledButton value={item.id} onClick={(event) => this.saveEdit(event)}>Save</StyledButton>
-                                        <StyledButton value={item.id} onClick={this.cancelEdit}>Cancel</StyledButton></td>
-                                    <td><input type="checkbox" value="true" /></td>
-                                    <td><button value={item.id}>Delete</button></td>
-                                </tr>
-                            }
-                            else
-                                return <tr key={item.id}>
-                                    <td>{item.item}</td>
-                                    <td><StyledButton value={item.id} onClick={() => this.editItem(item.id, item.item)}>Edit</StyledButton></td>
-                                    <td>
-                                        <input
-                                            onChange={() => {
-                                                this.updatePriority(item);
-                                            }}
-                                            type="checkbox"
-                                            checked={item.priority}
-                                        />
-                                    </td>
+                    <td>
+                      <RemoveButton onClick={() => this.deleteItem(item.id)}>
+                        Delete
+                      </RemoveButton>
+                    </td>
+                  </tr>
+                );
+            })}
+          </tbody>
+        </table>
+        <br></br>
+        <br></br>
+        <h2>Insert new item</h2>
 
-                                    <td>
-                                        <RemoveButton onClick={() => this.deleteItem(item.id)}>Delete</RemoveButton>
-                                    </td>
-                                </tr>
-
-                        })}
-                    </tbody>
-                </table>
-                <br></br>
-                <br></br>
-                <h2>Insert new item</h2>
-
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Select High Priority</th>
-                            <th>Save Item</th>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                            <td className="checkBox">
-                                <StyledCheckbox type="checkbox"></StyledCheckbox>
-                            </td>
-                            <td>
-                                <StyledButton onClick={() => this.addItem()}>
-                                    Save New Item
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Select High Priority</th>
+              <th>Save Item</th>
+            </tr>
+            <tr>
+              <td>
+                <input type="text"></input>
+              </td>
+              <td className="checkBox">
+                <StyledCheckbox type="checkbox"></StyledCheckbox>
+              </td>
+              <td>
+                <StyledButton onClick={() => this.addItem()}>
+                  Save New Item
                 </StyledButton>
               </td>
             </tr>
@@ -150,7 +179,7 @@ class WishListAdmin extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    state,
+  state,
 });
 
 export default connect(mapStateToProps)(WishListAdmin);
