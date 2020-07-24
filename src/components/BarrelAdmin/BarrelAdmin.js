db55e69b03af98b211555df30dc3ec12e173c0d9
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-24 18:49:55 -0500

    Souped up the form, and added a special button for Carolyn

--- src/components/BarrelAdmin/BarrelAdmin.js
+++ src/components/BarrelAdmin/BarrelAdmin.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./BarrelAdmin.css";
 import { StyledButton } from "../ButtonStyles/Buttons";
-
+import TextField from "@material-ui/core/TextField";
 class BarrelAdmin extends Component {
   state = {
     host: "",
@@ -25,84 +25,99 @@ class BarrelAdmin extends Component {
   };
   render() {
     return (
-      <div className="addBarrels">
-        <div className="addBarrelInput">
-          <label for="name"> Name:</label>
-          <input
-            name="name"
-            type="text"
-            value={this.state.name}
-            onChange={this.handleInput}
-          />
-        </div>
-        <div className="addBarrelInput">
-          <label for="Street">Street:</label>
+      <div className="barrelForm">
+        <h3 id="addDonationH3" className="tableTitle">
+          Add a Donation Location
+        </h3>
 
-          <input
-            name="street"
-            type="text"
-            value={this.state.street}
-            onChange={this.handleInput}
-          />
-        </div>
-        <div className="addBarrelInput">
-          <label for="City">City:</label>
-          <input
-            name="city"
-            type="text"
-            value={this.state.city}
-            onChange={this.handleInput}
-          />
-        </div>
-        <div className="addBarrelInput">
-          <label for="Zipcode">Zipcode:</label>
-          <input
-            name="zipcode"
-            type="text"
-            value={this.state.zipcode}
-            onChange={this.handleInput}
-          />
-        </div>
-        <div className="addBarrelInput">
-          <label for="Directions">Directions:</label>
-          <input
-            name="directions"
-            type="text"
-            value={this.state.directions}
-            onChange={this.handleInput}
-          />
-        </div>
-        <div className="addBarrelInput">
-          <label for="hours">Hours:</label>
-          <input
-            name="hours"
-            type="text"
-            value={this.state.hours}
-            onChange={this.handleInput}
-          />
-        </div>
-        <div className="addBarrelInput">
-          <label for="status">Status:</label>
-          <input
-            name="status"
-            type="text"
-            value={this.state.status}
-            onChange={this.handleInput}
-          />
-        </div>
-        <StyledButton
-          className="addBarrelButton"
-          onClick={() => this.addBarrel}
-        >
-          Submit
-        </StyledButton>
-        {/* <button
+        <form>
+          <div className="addBarrels">
+            <div className="addBarrelInput">
+              <TextField
+                label="Name"
+                variant="outlined"
+                name="name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleInput}
+              />
+            </div>
+            <div className="addBarrelInput">
+              <TextField
+                variant="outlined"
+                label="Street"
+                name="street"
+                type="text"
+                value={this.state.street}
+                onChange={this.handleInput}
+              />
+            </div>
+            <div className="addBarrelInput">
+              <TextField
+                label="City"
+                variant="outlined"
+                name="city"
+                type="text"
+                value={this.state.city}
+                onChange={this.handleInput}
+              />
+            </div>
+            <div className="addBarrelInput">
+              <TextField
+                label="Zipcode"
+                name="zipcode"
+                type="text"
+                variant="outlined"
+                value={this.state.zipcode}
+                onChange={this.handleInput}
+              />
+            </div>
+            <div className="addBarrelInput">
+              <TextField
+                label="Directions"
+                name="directions"
+                type="text"
+                variant="outlined"
+                value={this.state.directions}
+                onChange={this.handleInput}
+              />
+            </div>
+            <div className="addBarrelInput">
+              <TextField
+                label="Hours"
+                name="hours"
+                type="text"
+                variant="outlined"
+                value={this.state.hours}
+                onChange={this.handleInput}
+              />
+            </div>
+            {/* <div className="addBarrelInput">
+              <TextField
+                name="status"
+                type="text"
+                label="Status"
+                variant="outlined"
+                value={this.state.status}
+                onChange={this.handleInput}
+              />
+            </div> */}
+            <StyledButton
+              id="theSubmitButtonForCarolyn"
+              className="addBarrelButton"
+              onClick={() => this.addBarrel}
+            >
+              Submit
+            </StyledButton>
+            {/* <button
           onClick={() => {
             this.addBarrel;
           }}
         >
           Add Location
         </button> */}
+          </div>
+        </form>
       </div>
     );
   }
