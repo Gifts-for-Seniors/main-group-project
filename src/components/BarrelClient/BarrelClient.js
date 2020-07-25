eac199441ec8d1ff23671d8e1aef2304c9f7b62a
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-07-25 12:57:51 -0500

    working on displaying barrel list

--- src/components/BarrelClient/BarrelClient.js
+++ src/components/BarrelClient/BarrelClient.js
@@ -6,8 +6,27 @@ class BarrelClient extends Component {
     render() {
         return (
             <div>
+                <p>Covid 19 Notice: Please click here to deliver gift donations at our operations center in Minneapolis. Individual appointments are socially distanced and honor CDC guidelines. Other drop locations below! You may also browse our Wish List for gift ideas.
+                </p>
+
+
+                <ul>
+                    <div>
+                        {/* {this.props.state.newBarrelReducer.map(barrel => {
+                            <li>barrel.hosts</li> 
+                        })} */}
+                    </div>
+                </ul>
+
+
+
+
+
+
 
             </div>
+
+
         );
     }
 }
