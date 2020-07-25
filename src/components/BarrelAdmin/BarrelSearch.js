a15ca7e4744c767813ddf139de0a80069436c05d
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-24 21:51:53 -0500

    Committing to avoid catastrophe

--- src/components/BarrelAdmin/BarrelSearch.js
+++ src/components/BarrelAdmin/BarrelSearch.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyledButton } from "../ButtonStyles/Buttons";
 import TextField from "@material-ui/core/TextField";
-export default function searchBarrels() {
-  return (
-    <form>
-      <div className="searchBarrels">
-        <div className="addBarrelInput">
-          <span>
-            <TextField
-              id="searchField"
-              label="Search"
-              name="search"
-              type="text"
-              variant="outlined"
-            //   value={this.state.search}
-            //   onChange={this.handleInput}
-            />
-            <StyledButton id="searchButton">Search</StyledButton>
-          </span>
-        </div>
+
+class searchBarrels extends Component {
+  state = {
+    search: "",
+  };
+  handleInput = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+    console.log(this.state);
+  };
+
+  search = () => {
+    console.log("click");
+  };
+  render() {
+    return (
+      <div>
+        <h3 id="addDonationH3" className="tableTitle">
+          Search Current Donation Locations
+        </h3>
+        <form>
+          <div className="searchBarrels">
+            <div className="addBarrelInput">
+              <span>
+                <TextField
+                  id="searchField"
+                  label="Search"
+                  name="search"
+                  type="text"
+                  variant="outlined"
+                  value={this.state.search}
+                  onChange={this.handleInput}
+                />
+                <StyledButton onClick={this.search} id="searchButton">
+                  Search
+                </StyledButton>
+              </span>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
-  );
+    );
+  }
 }
+export default searchBarrels;
