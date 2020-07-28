29c781ebbe94949ef6bed5fc3b637fa5293a3862
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-07-28 10:25:47 -0500

    updated to display search results

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,6 +28,7 @@ class App extends Component {
     this.props.dispatch({ type: "FETCH_LIST" });
     this.props.dispatch({ type: "GET_BARRELS" });
     this.props.dispatch({ type: "GET_ADMIN_BARRELS" });
+    this.props.dispatch({type: "SEARCH_ALL_BARRELS", payload: '*all' });
   }
 
   render() {
