c6d3e9c12865851c0f742d5287e62810f457200c
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-08-07 18:51:46 -0500

    Componetized BarrelInputs

--- src/components/BarrelTable/BarrelTable.js
+++ src/components/BarrelTable/BarrelTable.js
@@ -10,6 +10,7 @@ import { confirmAlert } from "react-confirm-alert";
 import "react-confirm-alert/src/react-confirm-alert.css";
 import TableData from "./TableData.js";
 import BarrelHead from "./TableHead.js";
+import BarrelInputs from "./BarrelInputs.js";
 class BarrelTable extends Component {
   state = {
     itemToEdit: 0,
@@ -44,8 +45,6 @@ class BarrelTable extends Component {
     });
     console.log("ITEM IS", item.public);
   };
-
-
 
   cancelEdit = () => {
     this.setState({
@@ -150,171 +149,15 @@ class BarrelTable extends Component {
             {this.props.state.searchBarrels.map((item) => {
               if (item.id === this.state.itemToEdit) {
                 return (
-                  <tr>
-                    <td>
-                      {/* INPUT FIELDS */}
-                      {/* HOSTS */}
-                      <TextField
-                        autoFocus="true"
-                        className="editInput"
-                        placeholder="Host Name"
-                        type="text"
-                        multiline
-                        rowsMax={10}
-                        value={this.state.hosts}
-                        variant="outlined"
-                        onChange={(event) => this.trackEdit(event, "hosts")}
-                      ></TextField>
-                    </td>
-                    <td>
-                      {/* STREET */}
-                      <TextField
-                        multiline
-                        rowsMax={10}
-                        autoFocus="true"
-                        className="editInput"
-                        placeholder="Street Address"
-                        type="text"
-                        value={this.state.street}
-                        variant="outlined"
-                        onChange={(event) => this.trackEdit(event, "street")}
-                      ></TextField>
-                    </td>
-                    <td>
-                      {/* CITY */}
-                      <TextField
-                        autoFocus="true"
-                        className="editInput"
-                        type="text"
-                        rowsMax={10}
-                        multiline
-                        placeholder="City"
-                        value={this.state.city}
-                        variant="outlined"
-                        onChange={(event) => this.trackEdit(event, "city")}
-                      ></TextField>
-                    </td>
-                    <td>
-                      {/* ZIPCODE */}
-                      <TextField
-                        autoFocus="true"
-                        className="editInput"
-                        type="text"
-                        rowsMax={10}
-                        multiline
-                        placeholder="Zipcode"
-                        value={this.state.zipcode}
-                        variant="outlined"
-                        onChange={(event) => this.trackEdit(event, "zipcode")}
-                      ></TextField>
-                    </td>
-                    <td>
-                      {/* DATES */}
-                      <TextField
-                        autoFocus="true"
-                        className="editInput"
-                        type="text"
-                        rowsMax={10}
-                        multiline
-                        value={this.state.date}
-                        variant="outlined"
-                        onChange={(event) => this.trackEdit(event, "date")}
-                        placeholder="Dates Available"
-                      ></TextField>
-                    </td>
-                    <td>
-                      {/* HOURS */}
-                      <TextField
-                        autoFocus="true"
-                        className="editInput"
-                        type="text"
-                        rowsMax={10}
-                        multiline
-                        placeholder="Hours Open"
-                        value={this.state.hours}
-                        variant="outlined"
-                        onChange={(event) => this.trackEdit(event, "hours")}
-                      ></TextField>
-                    </td>
-                    <td>
-                      {/* DESCRIPTION */}
-                      <TextField
-                        autoFocus="true"
-                        className="editInput"
-                        type="text"
-                        rowsMax={10}
-                        multiline
-                        placeholder="Description"
-                        value={this.state.description}
-                        variant="outlined"
-                        onChange={(event) =>
-                          this.trackEdit(event, "description")
-                        }
-                      ></TextField>
-                    </td>
-                    {/* ENDS INPUT FIELDS */}
-                    <td>
-                      {/* TOGGLES */}
-                      <div
-                        className="sliderCheckbox"
-                        class="ui slider checkbox"
-                      >
-                        <input
-                          type="checkbox"
-                          onChange={() => {
-                            this.updateStatus(item);
-                          }}
-                          checked={item.status}
-                          name="newsletter"
-                        />{" "}
-                        <label className="sliderLabel">
-                          {item.status ? "Active" : "Not Active"}
-                        </label>
-                      </div>
-                    </td>
-                    <td>
-                      <div
-                        className="sliderCheckbox"
-                        class="ui slider checkbox"
-                      >
-                        <input
-                          type="checkbox"
-                          onChange={() => {
-                            console.log(item.public);
-                            this.updatePublic(item);
-                          }}
-                          checked={item.public}
-                          name="newsletter"
-                        />{" "}
-                        <label className="sliderLabel">
-                          {item.public ? "Public" : "Private"}
-                        </label>
-                      </div>
-                    </td>
-                    {/* ENDS TOGGLES */}
-                    <td>
-                      <i
-                        class="archive icon"
-                        value={item.id}
-                        onClick={() => this.saveChanges()}
-                      >
-                        <p>Save</p>
-                      </i>
-                    </td>
-                    <td>
-                      <i class="ban icon" onClick={this.cancelEdit}>
-                        <p>Cancel</p>
-                      </i>
-                    </td>
-                    <td>
-                      <i
-                        class="trash icon"
-                        onClick={() => this.submitAlert(item.id)}
-                      >
-                        <p>Delete</p>
-                      </i>
-                    </td>
-                  </tr>
+                  <BarrelInputs
+                    updateStatus={this.updateStatus}
+                    updatePublic={this.updatePublic}
+                    trackEdit={this.trackEdit}
+                    item={item}
+                    submitAlert={this.submitAlert}
+                    cancelEdit={this.cancelEdit}
+                    saveChanges={this.saveChanges}
+                  />
                 );
               } else {
                 // RETURN A TABLE WITH DATA
--- src/components/GoogleMap/BarrelInsetGoogleMap.js
+++ src/components/GoogleMap/BarrelInsetGoogleMap.js
@@ -14,8 +14,7 @@ class InsetGoogleMap extends Component {
     let imageSource = `https://maps.googleapis.com/maps/api/staticmap?
     center=Minneapolis, MN&zoom=20&size=400x400&maptype=roadmap&markers=color:blue%7label:${host}%7${street}+${zipcode}+${city}"
     &key=AIzaSyBbn4YPkDlirgm-dFmTs_YvN6h-lSD7MoE`;
-    let test = `https://maps.googleapis.com/maps/api/staticmap?center=${city}&zoom=10&size=350x350&markers=size:large%7Ccolor:blue%7C${street}+${zipcode}&key=AIzaSyBbn4YPkDlirgm-dFmTs_YvN6h-lSD7MoE
-`;
+    let test = `https://maps.googleapis.com/maps/api/staticmap?center=${city}&zoom=10&size=350x350&markers=size:large%7Ccolor:blue%7C${street}+${zipcode}&key=AIzaSyBbn4YPkDlirgm-dFmTs_YvN6h-lSD7MoE`;
     console.log(this.props.barrel);
     return (
       // RENDER A DEFAULT MAP WITH GIFTS FOR SENIORS LOCATION
