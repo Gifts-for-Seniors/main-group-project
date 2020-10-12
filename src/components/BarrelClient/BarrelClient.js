a80358e86a7635421e6080d00b1f50f0d3c62b2b
Sudhansu.Singh <sudhansu.singh@target.com>
2020-10-12 17:43:28 -0500

    Merged code for barrel location

--- src/components/BarrelClient/BarrelClient.js
+++ src/components/BarrelClient/BarrelClient.js
@@ -1,36 +1,53 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import BarrelSearch from "../BarrelSearch/BarrelSearch";
-import "./BarrelClient.css";
-import GoogleMap from "../GoogleMap/GoogleMap";
-import InsetGoogleMap from "../GoogleMap/BarrelInsetGoogleMap";
-import BarrelCard from "../BarrelCard/BarrelCard";
-import { Button, Card, Image, Label, Grid } from "semantic-ui-react";
-import CovidReponse from "./CovidResponse";
-import Footer from "../Footer/Footer";
+import React, { Component } from 'react';
+import BarrelLocationsMap from '../BarrelLocationsMap/BarrelLocations';
+import ZipcodeFilter from '../ZipcodeFilter/ZipcodeFilter';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { connect } from 'react-redux';
+import BarrelSearch from '../BarrelSearch/BarrelSearch';
+import './BarrelClient.css';
+import GoogleMap from '../GoogleMap/GoogleMap';
+import InsetGoogleMap from '../GoogleMap/BarrelInsetGoogleMap';
+import BarrelCard from '../BarrelCard/BarrelCard';
+import { Button, Card, Image, Label, Grid } from 'semantic-ui-react';
+import CovidReponse from './CovidResponse';
+import Footer from '../Footer/Footer';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
 
 class BarrelClient extends Component {
   componentDidMount() {
     let homeBase = {
-      street: "2300 Kennedy Street",
-      zipcode: "55413",
+      street: '2300 Kennedy Street',
+      zipcode: '55413',
     };
     this.props.dispatch({
-      type: "SET_MAP_TO_SEARCH",
+      type: 'SET_MAP_TO_SEARCH',
       payload: homeBase,
     });
+
+    // this.getMap(this.props.state.searchBarrels);
   }
+
+  getBarrelLocationsMap = (barrelLocations) => {
+    // console.log('Barrel Locations', barrelLocations);
+    // const googleMapJSX;
+    // return googleMapJSX;
+  };
 
   // UPDATE MAP WITH TARGET INFORMATION (*barrel)
   setMapToDisplay = (barrel) => {
-    console.log(barrel);
+    // console.log(barrel);
     let data = {
       hosts: barrel.hosts,
       street: barrel.street,
       zipcode: barrel.zipcode,
     };
+
     this.props.dispatch({
-      type: "SET_MAP_TO_SEARCH",
+      type: 'SET_MAP_TO_SEARCH',
       payload: data,
     });
   };
@@ -38,31 +55,34 @@ class BarrelClient extends Component {
   /// }
   render() {
     return (
-      <div>
-        {" "}
-        <CovidReponse />
-        <BarrelSearch />
-        <Grid columns="equal" stackable>
-          <Grid.Row streched={true} columns={4}>
-            {/* ONLY DISPLAY RELEVANT INFORMATION */}
-            {this.props.state.searchBarrels.map((barrel) => {
-              if (barrel.status === true) {
-                // RETURN BARREL CARDS
-                return (
-                  <Grid.Column>
-                    <BarrelCard
-                      barrel={barrel}
-                      setMapToDisplay={this.setMapToDisplay}
-                    />
-                  </Grid.Column>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </Grid.Row>
-        </Grid>
-        <GoogleMap />
+      <div className='barrel-locations'>
+        <div className='barrel-header'>
+          <h1>FIND A BARREL</h1>
+          <h3>
+            Find a blue donation barrel location on the map nearest you. Donate
+            today to impact someones life tomorrow.
+          </h3>
+
+          <p className='set-appointment-text'>
+            We invite you to drop off your gift donations at our operations
+            center in Minneapolis. Appointments are social distanced and follow
+            CDC guidelines. To set up a contact time please contact us via
+            email!
+          </p>
+          <div className='contact-us'>
+            <a
+              className='emailAdd'
+              href='mailto:carolyn@giftsforseniors.org'
+              target='_blank'
+            >
+              EMAIL US
+            </a>
+          </div>
+        </div>
+        <BarrelLocationsMap
+          barrelLocations={this.props.state.searchBarrels}
+        ></BarrelLocationsMap>
+        <ZipcodeFilter barrelLocations={this.props.state.searchBarrels}></ZipcodeFilter>
         <Footer />
       </div>
     );
