04af2abb3c50e07c4b94ed61e21d33085a47aeae
Jason Nelson <Jasonn318@gmail.com>
2020-10-20 10:19:48 -0500

    added zipcode api request with zipCode route on server side to eliminate CORS error

---
+++ server/routes/barrel-api-zip-codes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const axios = require('axios');
+
+const router = express.Router();
+
+router.get('/:zipcode', (req, res) => {
+  axios.get(
+    `https://www.zipcodeapi.com/rest/hbabUJNFqBHwV2XO0hEriOeZp2L1kmhzsRoLsmn1bK5UXRtFRKqClVeWPdnYmxeV/radius.json/${req.params.zipcode}/3/mile`
+  ).then(resp => {
+    res.json(resp.data.zip_codes);
+  }).catch(e => {
+    console.log(e.response);
+  });
+});
+
+module.exports = router;
