a1f5d3d264c9c892bcf9bc4627d9975b4eda5ba5
Jason Nelson <Jasonn318@gmail.com>
2020-11-10 18:12:04 -0600

    added fallback zipcode API in case reaches more than 10 per hour

--- server/routes/barrel-api-zip-codes.js
+++ server/routes/barrel-api-zip-codes.js
@@ -9,8 +9,20 @@ router.get('/:zipcode', (req, res) => {
   ).then(resp => {
     res.json(resp.data.zip_codes);
   }).catch(e => {
-    console.log(e.response);
-    res.status(400).send(new Error(e.toString()));
+    axios.get(`https://www.zipwise.com/webservices/radius.php?key=chupcauyzztykpb3&zip=${req.params.zipcode}&radius=20&format=json`).then(resp => {
+      let resultArr = resp.data.results.map(zipObj => {
+        return {
+          distance: Number(zipObj.distance),
+          zip_code: zipObj.zip,
+          city: zipObj.city,
+          state: zipObj.state
+        };
+      });
+
+      res.json(resultArr);
+    }).catch(e => {
+      res.status(400).send(new Error(e.toString()));
+    });
   });
 });
 
